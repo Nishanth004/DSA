ccc9a183be5a2eb57a51e71431ca387968297909
Nishanth M <126948550+Nishanth004@users.noreply.github.com>
2024-08-09 23:20:14 +0530

    Create 7. Reverse Integer.cpp

---
+++ leetcode/7. Reverse Integer.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int reverse(int n) {
+        int r;
+        long sum=0;
+        while(n!=0)
+        {
+            int r=n%10;
+            sum=sum*10+r;
+            n/=10;
+        }
+        if(sum>INT_MAX||sum<INT_MIN)
+            return 0;
+        return sum;
+    }
+};
