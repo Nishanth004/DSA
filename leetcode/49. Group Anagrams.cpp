6c9d4f1295d45f86e812b1cecd4b8d001292828c
Nishanth M <126948550+Nishanth004@users.noreply.github.com>
2024-10-25 18:34:16 +0530

    Create 49. Group Anagrams.cpp

---
+++ leetcode/49. Group Anagrams.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    vector<vector<string>> groupAnagrams(vector<string>& strs) {
+        vector<vector<string>>ans;
+        unordered_map<string, vector<string>>map;
+        
+        for(auto str:strs)
+        {
+            string s=str;
+            sort(str.begin(), str.end());
+            map[str].push_back(s);
+        }
+
+        for( auto group:map)
+        ans.push_back(group.second);
+            return ans;
+        
+    }
+};
