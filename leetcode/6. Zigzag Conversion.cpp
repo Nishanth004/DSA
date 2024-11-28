c7c9869f61978032e5fc7d57d478410668564bee
Nishanth M <126948550+Nishanth004@users.noreply.github.com>
2024-11-28 18:50:51 +0530

    Create 6. Zigzag Conversion.cpp

---
+++ leetcode/6. Zigzag Conversion.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    string convert(string s, int num) {
+        int n=s.size();
+        if(num==1 || num>n)
+            return s;
+        vector<vector<char>> mat(num);
+        int row=0, d=1;
+        for(char c:s)
+        {
+            mat[row].push_back(c);
+            if(row==0)
+                d=1;
+            else if(row==num-1)
+                d=-1;
+            row=row+d;
+        }
+        string result;
+        for(auto & row:mat)
+        {
+            for(char c:row){
+                result+=c;
+            }
+        }
+        return result;
+
+    }
+};
